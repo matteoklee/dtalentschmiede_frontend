c9e092c1dad6d0948cd3f27b6980133793dd2e92
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2024-10-18 01:14:38 +0200

    working on design for adding projects

--- src/components/project2/ProjectAddDrawer.vue
+++ src/components/project2/ProjectAddDrawer.vue
@@ -16,35 +16,128 @@
     </div>
 
     <!-- drawer component -->
-    <!-- bg-black bg-gradient-to-r from-purple-800/50 from-10% via-indigo-700/50 via-30% to-blue-900/50 to-90% -->
+    <!-- bg-black bg-gradient-to-r from-purple-800/50 from-10% via-indigo-700/50 via-30% to-blue-900/50 to-90%     bg-slate-900 -->
     <div
         id="drawer-full-example"
-        class="flex flex-col fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto bg-gray-50 w-full translate-x-full bg-slate-900"
+        class="flex flex-col fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto bg-gray-50 w-full translate-x-full bg-gray-50"
         tabindex="-1"
         aria-labelledby="drawer-full-label"
     >
-      <div class="w-3/4 mx-auto flex flex-col">
-        <button
-            type="button"
-            @click="closeDrawer"
-            data-drawer-hide="drawer-full-example"
-            aria-controls="drawer-full-example"
-            class="text-gray-400 bg-transparent hover:text-gray-200 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center"
-        >
-          <IconClose class="h-5 w-5"></IconClose>
-          <span class="sr-only">Close menu</span>
-        </button>
 
-        <div class="mb-8">
+      <div class="m-4 max-w-screen-xl mx-auto flex flex-row justify-between items-center">
+        <div class="">
           <HeaderLogo class=""></HeaderLogo>
         </div>
 
+        <div>
+          <button
+              type="button"
+              @click="closeDrawer"
+              data-drawer-hide="drawer-full-example"
+              aria-controls="drawer-full-example"
+              class="text-gray-400 bg-transparent hover:text-gray-800 rounded-lg text-sm w-8 h-8 inline-flex items-center justify-center"
+          > <!-- absolute top-2.5 end-2.5  -->
+            <IconClose class="h-8 w-8"></IconClose>
+            <span class="sr-only">Close menu</span>
+          </button>
+        </div>
 
-        <div class="flex flex-col justify-center items-center">
+        <div class="hidden flex flex-col justify-center items-center">
+          <div class="w-3/4 mx-auto p-6 rounded-lg m-4"> <!-- bg-indigo-900/20 text-gray-400 border border-gray-600 -->
 
-          <div class="w-1/2 h-96 mx-auto bg-indigo-900/20 text-gray-400 border border-gray-600 p-6 rounded-lg m-4">
+            <!--
+            <div class="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg">
+              <h2 class="text-2xl font-bold mb-4">Neues IT-Projekt anlegen</h2>
 
-            <div>
+              <form @submit.prevent="submitForm">
+                <div class="mb-4">
+                  <label for="title" class="block text-sm font-medium text-gray-700">Projekttitel</label>
+                  <input
+                      v-model="project.title"
+                      type="text"
+                      id="title"
+                      class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      placeholder="Geben Sie den Projekttitel ein"
+                      required
+                  />
+                </div>
+
+                <div class="mb-4">
+                  <label for="description" class="block text-sm font-medium text-gray-700">Projektbeschreibung</label>
+                  <textarea
+                      v-model="project.description"
+                      id="description"
+                      rows="4"
+                      class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      placeholder="Geben Sie eine kurze Beschreibung des Projekts ein"
+                      required
+                  ></textarea>
+                </div>
+
+                <div class="mb-4">
+                  <label for="technologies" class="block text-sm font-medium text-gray-700">Technologien</label>
+                  <input
+                      v-model="project.technologies"
+                      type="text"
+                      id="technologies"
+                      class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      placeholder="Z.B. Java, Python, Vue.js"
+                      required
+                  />
+                </div>
+
+                <div class="mb-4">
+                  <label for="scope" class="block text-sm font-medium text-gray-700">Projektumfang</label>
+                  <input
+                      v-model="project.scope"
+                      type="text"
+                      id="scope"
+                      class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      placeholder="Z.B. 3 Monate, 200 Stunden"
+                      required
+                  />
+                </div>
+
+                <div class="mb-4">
+                  <label for="startDate" class="block text-sm font-medium text-gray-700">Startdatum</label>
+                  <input v-model="project.startDate"
+                         type="text"
+                         id="startDate"
+                         datepicker datepicker-buttons datepicker-autoselect-today
+                         class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                         placeholder="Select date">
+
+                </div>
+
+                <div class="mb-4">
+                  <label for="supervisor" class="block text-sm font-medium text-gray-700">Betreuer</label>
+                  <input
+                      v-model="project.supervisor"
+                      type="text"
+                      id="supervisor"
+                      class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      placeholder="Name des Betreuers"
+                      required
+                  />
+                </div>
+
+                <div class="flex justify-end">
+                  <button
+                      type="submit"
+                      class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Projekt anlegen
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          -->
+          </div>
+          <!--
+          <div class="w-1/2 mx-auto bg-indigo-900/20 text-gray-400 border border-gray-600 p-6 rounded-lg m-4">
+
+            <div class="">
               <div class="text-xl text-red-600">
                 <Typer
                     :text="['Willkommen in der dProjektbörse!\nLass uns direkt loslegen!']"
@@ -81,17 +174,62 @@
               </div>
             </div>
 
+          </div> -->
+        </div>
+      </div>
+
+      <div class="grid lg:grid-cols-2 grid-cols-1 gap-4 mx-auto">
+        <div class="flex flex-col justify-center items-start">
+          <div>
+            <p class="text-5xl font-bold text-left py-4">Erstelle ein neues <span class="text-primary-700">IT-Projekt</span>.</p>
+            <p class="py-2 text-lg">Work remotely to companies in worldwide</p>
           </div>
 
+          <div class="w-full">
+            <div
+                class="flex items-center space-x-2 bg-white py-4 my-4 px-4 rounded-lg shadow-md text-gray-600 hover:scale-100 duration-300"
+            >
+              <div class="flex items-center space-x-2 w-full relative mr-2">
+                <IconFile class="h-5 text-gray-400"></IconFile>
+                <input
+                    v-model="searchQuery"
+                    @input="searchProject"
+                    type="text"
+                    id="projectSearch"
+                    placeholder="Titel des Projekts"
+                    required
+                    class="border-0 text-lg w-full outline-none focus:ring-0 placeholder-gray-300 pr-6"
+                />
+                <button type="button" @click="clear" class="absolute right-0">
+                  <IconClose class="h-5 w-5 text-gray-400 hover:text-gray-600"></IconClose>
+                </button>
+              </div>
+              <a
+                  type="button"
+                  href="#projects"
+                  class="bg-primary-600 shadow-lg shadow-primary-600/50 px-6 py-3 rounded-lg text-white hover:bg-primary-700 transition"
+              >Bestätigen</a
+              >
+            </div>
+
+            <p class="text-gray-400 font-normal text-md py-4">
+              Example:
+              <span class="border px-3 py-1 mx-2 rounded-md">Idee</span>
+              <span class="border px-3 py-1 mx-2 rounded-md">Abschlussarbeit</span>
+              <span class="border px-3 py-1 mx-2 rounded-md">Projekt</span>
+            </p>
+
+          </div>
         </div>
-
-
-
-        <div class="text-center mt-12">
-          <FooterInfo></FooterInfo>
-          <FooterNav class="text-blue-400"></FooterNav>
-          <FooterCopyright></FooterCopyright>
+        <div>
+          <img src="@/assets/img_5.png" />
         </div>
+      </div>
+
+      <div class="text-center mt-12">
+        <FooterInfo></FooterInfo>
+        <FooterNav></FooterNav>
+        <FooterCopyright></FooterCopyright>
       </div>
 
     </div>
@@ -108,18 +246,42 @@ import FooterCopyright from "@/components/navigation/footer/FooterCopyright.vue"
 import FooterNav from "@/components/navigation/footer/FooterNav.vue";
 import FooterInfo from "@/components/navigation/footer/FooterInfo.vue";
 import IconCheck from "@/components/project2/icons/IconCheck.vue";
+import IconFile from "@/components/project2/icons/IconFile.vue";
 export default {
   name: "ProjectAddDrawer",
-  components: {IconCheck, FooterInfo, FooterNav, FooterCopyright, HeaderLogo, IconClose, IconRight, Typer},
+  components: {IconFile, IconCheck, FooterInfo, FooterNav, FooterCopyright, HeaderLogo, IconClose, IconRight, Typer},
   data() {
     return {
       email: '',
       emailValidated: false,
+
+      project: {
+        title: '',
+        description: '',
+        technologies: '',
+        scope: '',
+        startDate: new Date().toISOString().split('T')[0],
+        supervisor: ''
+      }
     }
   },
   methods: {
     validateEmail() {
       this.emailValidated = this.email !== '';
+    },
+    submitForm() {
+      // Sende die Projektdaten, z.B. an eine API oder speichere lokal
+      console.log('Projekt wurde angelegt:', this.project);
+      alert('Projekt erfolgreich angelegt!');
+      // Formular zurücksetzen
+      this.project = {
+        title: '',
+        description: '',
+        technologies: '',
+        scope: '',
+        startDate: new Date().toISOString().split('T')[0],
+        supervisor: ''
+      };
     }
   },
   mounted() {
